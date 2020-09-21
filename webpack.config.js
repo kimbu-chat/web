206c1b6ba70863cf121f1ec9fd4e5f6cc0a296c7
Stef <sstefstefv@gmail.com>
2020-09-21 20:41:31 +0300

    Lastcommit in this branch

--- webpack.config.js
+++ webpack.config.js
@@ -84,7 +84,6 @@ module.exports = {
 					{
 						loader: 'postcss-loader',
 						options: {
-							ident: 'postcss',
 							postcssOptions: {
 								plugins: [
 									require('postcss-import')({ addDependencyTo: webpack }),
@@ -126,19 +125,20 @@ module.exports = {
 					{
 						loader: 'postcss-loader',
 						options: {
-							ident: 'postcss',
-							plugins: [
-								require('postcss-import')({ addDependencyTo: webpack }),
-								require('postcss-url')(),
-								require('postcss-preset-env')({
-									/* use stage 2 features (defaults) */
-									stage: 2,
-								}),
-								require('postcss-reporter')(),
-								require('postcss-browser-reporter')({
-									disabled: isProduction,
-								}),
-							],
+							postcssOptions: {
+								plugins: [
+									require('postcss-import')({ addDependencyTo: webpack }),
+									require('postcss-url')(),
+									require('postcss-preset-env')({
+										/* use stage 2 features (defaults) */
+										stage: 2,
+									}),
+									require('postcss-reporter')(),
+									require('postcss-browser-reporter')({
+										disabled: isProduction,
+									}),
+								],
+							},
 						},
 					},
 				],

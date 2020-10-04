322669d447daf78229f7c1d1756fc46a098d9ca2
Stef <sstefstefv@gmail.com>
2020-10-04 15:09:42 +0300

    Logic moved in order to avoid props drilling

--- src/app/components/messenger-page/chat-info/chat-actions/chat-actions.tsx
+++ src/app/components/messenger-page/chat-info/chat-actions/chat-actions.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useCallback, useContext, useState } from 'react';
 import './chat-actions.scss';
 import { UserPreview } from 'app/store/my-profile/models';
 import { Chat } from 'app/store/chats/models';
@@ -6,42 +6,44 @@ import { useSelector } from 'react-redux';
 import { getSelectedChatSelector } from 'app/store/chats/selectors';
 import { RootState } from 'app/store/root-reducer';
 import { LocalizationContext } from 'app/app';
+import { ChatActions as DialogActions } from 'app/store/chats/actions';
 
 //svg
 import MuteSvg from 'app/assets/icons/ic-notifications-on.svg';
-import UnmuteSvg from 'app/assets/icons/ic-notifications-on.svg';
+import UnmuteSvg from 'app/assets/icons/ic-notifications-off.svg';
 import ClearSvg from 'app/assets/icons/ic-clear.svg';
 import EditSvg from 'app/assets/icons/ic-edit.svg';
 import DeleteSvg from 'app/assets/icons/ic-delete.svg';
 import LeaveSvg from 'app/assets/icons/ic-leave-chat.svg';
+import Modal from 'app/components/shared/modal/modal';
+import WithBackground from 'app/components/shared/with-background';
+import { useActionWithDispatch } from 'app/utils/use-action-with-dispatch';
+import { useActionWithDeferred } from 'app/utils/use-action-with-deferred';
+import { FriendActions } from 'app/store/friends/actions';
 
 namespace ChatActions {
 	export interface Props {
-		muteChat: () => void;
-		createConference?: () => void;
-		deleteContact?: () => void;
-		deleteChat?: () => void;
-		deleteConference?: () => void;
 		addMembers: (params: { excludeIds: (number | undefined)[] }) => void;
+		displayCreateChat: () => void;
 	}
 }
 
-const ChatActions = ({
-	muteChat,
-	createConference,
-	deleteContact,
-	deleteChat,
-	deleteConference,
-	addMembers,
-}: ChatActions.Props) => {
+const ChatActions = ({ addMembers, displayCreateChat }: ChatActions.Props) => {
 	const { t } = useContext(LocalizationContext);
+
+	const [leaveConferenceModalOpened, setLeaveConferenceModalOpened] = useState<boolean>(false);
+
+	const removeChat = useActionWithDispatch(DialogActions.removeChat);
+	const muteChat = useActionWithDispatch(DialogActions.muteChat);
+	const deleteFriend = useActionWithDispatch(FriendActions.deleteFriend);
+	const markUser = useActionWithDispatch(FriendActions.markUserAsAddedToConference);
+
+	const leaveConference = useActionWithDeferred(DialogActions.leaveConference);
 
 	const membersForConference = useSelector<RootState, UserPreview[]>(
 		(state) => state.friends.usersForSelectedConference,
 	);
-
 	const membersIdsForConference: (number | undefined)[] = membersForConference.map((user) => user?.id);
-
 	const selectedChat = useSelector(getSelectedChatSelector) as Chat;
 	const friends = useSelector<RootState, UserPreview[]>((state) => state.friends.friends);
 
@@ -49,10 +51,28 @@ const ChatActions = ({
 		return friends.findIndex((friend: UserPreview) => friend.id === selectedChat.interlocutor?.id) > -1;
 	};
 
+	const deleteChat = useCallback(() => removeChat(selectedChat), [removeChat, selectedChat]);
+	const changeLeaveConferenceModalOpenedState = useCallback(
+		() => setLeaveConferenceModalOpened((oldState) => !oldState),
+		[setLeaveConferenceModalOpened],
+	);
+	const deleteConference = useCallback(async () => {
+		await leaveConference(selectedChat);
+	}, [leaveConference, changeLeaveConferenceModalOpenedState, selectedChat]);
+	const muteThisChat = useCallback(() => muteChat(selectedChat), [muteChat, selectedChat]);
+	const deleteContact = useCallback(() => deleteFriend({ userIds: [selectedChat?.interlocutor?.id || -1] }), [
+		deleteFriend,
+		selectedChat?.interlocutor?.id,
+	]);
+	const createConference = useCallback(() => {
+		displayCreateChat();
+		markUser(selectedChat.interlocutor?.id || -1);
+	}, [displayCreateChat, markUser, selectedChat?.interlocutor?.id]);
+
 	return (
 		<div className='chat-actions'>
 			<div className='chat-actions__heading'>{t('chatActions.actions')}</div>
-			<button onClick={muteChat} className='chat-actions__action'>
+			<button onClick={muteThisChat} className='chat-actions__action'>
 				{selectedChat.isMuted ? (
 					<UnmuteSvg className='chat-actions__action__svg' />
 				) : (
@@ -72,13 +92,13 @@ const ChatActions = ({
 					<span className='chat-actions__action__name'>{t('chatActions.edit-contact')}</span>
 				</button>
 			)}
-			{selectedChat.interlocutor && selectedIsFriend && (
+			{selectedChat.interlocutor && selectedIsFriend() && (
 				<button onClick={deleteContact} className='chat-actions__action'>
 					<DeleteSvg className='chat-actions__action__svg' />
 					<span className='chat-actions__action__name'>{t('chatActions.delete-contact')}</span>
 				</button>
 			)}
-			{selectedChat.interlocutor && selectedIsFriend && (
+			{selectedChat.interlocutor && selectedIsFriend() && (
 				<button onClick={createConference} className='chat-actions__action'>
 					<UnmuteSvg className='chat-actions__action__svg' />
 					<span className='chat-actions__action__name'>{t('chatActions.create-group')}</span>
@@ -91,7 +111,7 @@ const ChatActions = ({
 				</button>
 			)}
 			{selectedChat.conference && (
-				<button onClick={deleteConference} className='chat-actions__action'>
+				<button onClick={changeLeaveConferenceModalOpenedState} className='chat-actions__action'>
 					<LeaveSvg className='chat-actions__action__svg' />
 					<span className='chat-actions__action__name'>{t('chatActions.leave-chat')}</span>
 				</button>
@@ -105,6 +125,50 @@ const ChatActions = ({
 					<span className='chat-actions__action__name'>{t('chatActions.add-users')}</span>
 				</button>
 			)}
+
+			{
+				//!Modal rendered with portal
+			}
+
+			<WithBackground
+				isBackgroundDisplayed={leaveConferenceModalOpened}
+				onBackgroundClick={changeLeaveConferenceModalOpenedState}
+			>
+				{leaveConferenceModalOpened && (
+					<Modal
+						title='Delete chat'
+						contents={t('chatInfo.leave-confirmation', { conferenceName: selectedChat.conference?.name })}
+						highlightedInContents={`‘${selectedChat.conference?.name}‘`}
+						closeModal={changeLeaveConferenceModalOpenedState}
+						buttons={[
+							{
+								text: t('chatInfo.confirm'),
+								style: {
+									color: 'rgb(255, 255, 255)',
+									backgroundColor: 'rgb(209, 36, 51)',
+									padding: '16px 49.5px',
+									margin: '0',
+								},
+								position: 'left',
+								onClick: deleteConference,
+							},
+							{
+								text: t('chatInfo.cancel'),
+								style: {
+									color: 'rgb(109, 120, 133)',
+									backgroundColor: 'rgb(255, 255, 255)',
+									padding: '16px 38px',
+									margin: '0 0 0 10px',
+									border: '1px solid rgb(215, 216, 217)',
+								},
+
+								position: 'left',
+								onClick: changeLeaveConferenceModalOpenedState,
+							},
+						]}
+					/>
+				)}
+			</WithBackground>
 		</div>
 	);
 };
--- src/app/components/messenger-page/chat-info/chat-info.tsx
+++ src/app/components/messenger-page/chat-info/chat-info.tsx
@@ -1,9 +1,8 @@
-import React, { useState, useRef, useCallback, useContext, useEffect } from 'react';
+import React, { useState, useRef, useCallback, useEffect } from 'react';
 import './chat-info.scss';
 import { useSelector } from 'react-redux';
 import { Chat } from 'app/store/chats/models';
 import { getInterlocutorInitials, getChatInterlocutor } from '../../../utils/interlocutor-name-utils';
-import { useActionWithDispatch } from 'app/utils/use-action-with-dispatch';
 import { useActionWithDeferred } from 'app/utils/use-action-with-deferred';
 import { Messenger } from 'app/containers/Messenger/Messenger';
 import { AvatarSelectedData } from 'app/store/my-profile/models';
@@ -17,13 +16,9 @@ import ChatMedia from './chat-media/chat-media';
 import ChatPhoto from './chat-photo/chat-photo';
 import ChatVideo from './chat-video/chat-video';
 
-import { FriendActions } from 'app/store/friends/actions';
 import { ChatActions } from 'app/store/chats/actions';
 
 import Avatar from 'app/components/shared/avatar/avatar';
-import Modal from 'app/components/shared/modal/modal';
-import WithBackground from 'app/components/shared/with-background';
-import { LocalizationContext } from 'app/app';
 
 import EditSvg from 'app/assets/icons/ic-edit.svg';
 import PhotoSvg from 'app/assets/icons/ic-photo.svg';
@@ -48,8 +43,6 @@ const ChatInfo: React.FC<ChatInfo.Props> = ({
 	displayChangePhoto,
 	isDisplayed,
 }) => {
-	const { t } = useContext(LocalizationContext);
-
 	const [chatPhotoDisplayed, setChatPhotoDisplayed] = useState(false);
 	const closeChatPhoto = useCallback(() => setChatPhotoDisplayed(false), [setChatPhotoDisplayed]);
 	const displayChatPhoto = useCallback(() => setChatPhotoDisplayed(true), [setChatPhotoDisplayed]);
@@ -72,38 +65,9 @@ const ChatInfo: React.FC<ChatInfo.Props> = ({
 
 	const selectedChat = useSelector(getSelectedChatSelector) as Chat;
 
-	const leaveConference = useActionWithDeferred(ChatActions.leaveConference);
-	const removeChat = useActionWithDispatch(ChatActions.removeChat);
-	const muteChat = useActionWithDispatch(ChatActions.muteChat);
-	const deleteFriend = useActionWithDispatch(FriendActions.deleteFriend);
-	const markUser = useActionWithDispatch(FriendActions.markUserAsAddedToConference);
 	const addUsersToConferece = useActionWithDeferred(ChatActions.addUsersToConference);
 	const changeConferenceAvatar = useActionWithDeferred(ChatActions.changeConferenceAvatar);
 
-	const [leaveConferenceModalOpened, setLeaveConferenceModalOpened] = useState<boolean>(false);
-
-	const openLeaveConferenceModal = useCallback(() => setLeaveConferenceModalOpened(true), [
-		setLeaveConferenceModalOpened,
-	]);
-	const closeLeaveConferenceModal = useCallback(() => setLeaveConferenceModalOpened(false), [
-		setLeaveConferenceModalOpened,
-	]);
-
-	const deleteChat = useCallback(() => removeChat(selectedChat), [removeChat, selectedChat]);
-	const muteThisChat = useCallback(() => muteChat(selectedChat), [muteChat, selectedChat]);
-	const deleteContact = useCallback(() => deleteFriend({ userIds: [selectedChat?.interlocutor?.id || -1] }), [
-		deleteFriend,
-		selectedChat?.interlocutor?.id,
-	]);
-	const deleteConference = useCallback(async () => {
-		await leaveConference(selectedChat);
-		closeLeaveConferenceModal();
-	}, [leaveConference, closeLeaveConferenceModal, selectedChat]);
-
-	const createConference = useCallback(() => {
-		displayCreateChat();
-		markUser(selectedChat.interlocutor?.id || -1);
-	}, [displayCreateChat, markUser, selectedChat?.interlocutor?.id]);
 	const addUsers = useCallback(
 		(userIds: number[]): void => {
 			addUsersToConferece({ chat: selectedChat, userIds }).then(hideContactSearch);
@@ -200,14 +164,7 @@ const ChatInfo: React.FC<ChatInfo.Props> = ({
 
 					<InterlocutorInfo />
 
-					<ChatInfoActions
-						deleteChat={deleteChat}
-						deleteConference={openLeaveConferenceModal}
-						muteChat={muteThisChat}
-						deleteContact={deleteContact}
-						createConference={createConference}
-						addMembers={searchContactsToAdd}
-					/>
+					<ChatInfoActions displayCreateChat={displayCreateChat} addMembers={searchContactsToAdd} />
 
 					<ChatMedia displayChatPhoto={displayChatPhoto} displayChatVideo={displayChatVideo} />
 
@@ -219,46 +176,6 @@ const ChatInfo: React.FC<ChatInfo.Props> = ({
 				<ChatVideo isDisplayed={chatVideoDisplayed} close={closeChatVideo} />
 
 				<EditChatModal isDisplayed={editConferenceDisplayed} close={changeEditConferenceDisplayedState} />
-
-				<WithBackground
-					isBackgroundDisplayed={leaveConferenceModalOpened}
-					onBackgroundClick={closeLeaveConferenceModal}
-				>
-					{leaveConferenceModalOpened && (
-						<Modal
-							title='Delete chat'
-							contents={t('chatInfo.leave-confirmation', { conferenceName: conference?.name })}
-							highlightedInContents={`‘${conference?.name}‘`}
-							closeModal={closeLeaveConferenceModal}
-							buttons={[
-								{
-									text: t('chatInfo.confirm'),
-									style: {
-										color: 'rgb(255, 255, 255)',
-										backgroundColor: 'rgb(209, 36, 51)',
-										padding: '16px 49.5px',
-										margin: '0',
-									},
-									position: 'left',
-									onClick: deleteConference,
-								},
-								{
-									text: t('chatInfo.cancel'),
-									style: {
-										color: 'rgb(109, 120, 133)',
-										backgroundColor: 'rgb(255, 255, 255)',
-										padding: '16px 38px',
-										margin: '0 0 0 10px',
-										border: '1px solid rgb(215, 216, 217)',
-									},
-
-									position: 'left',
-									onClick: closeLeaveConferenceModal,
-								},
-							]}
-						/>
-					)}
-				</WithBackground>
 			</React.Fragment>
 		);
 	} else {

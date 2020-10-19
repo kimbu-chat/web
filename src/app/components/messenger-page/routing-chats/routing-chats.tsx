6c3c54b4692fdb10cde306ec203e017ce2724bf4
eternalEagle <52489178+eternalEagle@users.noreply.github.com>
2020-10-19 21:39:12 +0300

    Routing fixed

--- src/app/components/messenger-page/routing-chats/routing-chats.tsx
+++ src/app/components/messenger-page/routing-chats/routing-chats.tsx
@@ -6,8 +6,11 @@ import ContactSvg from 'app/assets/icons/ic-contacts.svg';
 import CallSvg from 'app/assets/icons/ic-call-filled.svg';
 import ChatsSvg from 'app/assets/icons/ic-chat.svg';
 import SettingsSvg from 'app/assets/icons/ic-settings.svg';
+import { getSelectedChatSelector } from 'app/store/chats/selectors';
+import { useSelector } from 'react-redux';
 
 const RoutingChats = () => {
+	const selectedChatId = useSelector(getSelectedChatSelector)?.id;
 	return (
 		<div className='routing-chats'>
 			<NavLink
@@ -27,7 +30,7 @@ const RoutingChats = () => {
 			<NavLink
 				className='routing-chats__link'
 				activeClassName='routing-chats__link routing-chats__link--active'
-				to='/chats'
+				to={selectedChatId ? `/chats/${selectedChatId}` : '/chats'}
 			>
 				<ChatsSvg viewBox='0 0 25 25' />
 			</NavLink>

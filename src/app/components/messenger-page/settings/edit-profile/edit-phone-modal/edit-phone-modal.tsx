17fb90d2d529d4e941788aafbb3d28c280706992
eternalEagle <52489178+eternalEagle@users.noreply.github.com>
2020-10-21 21:59:59 +0300

    Edit phone modal pixel perfect

--- src/app/components/messenger-page/settings/edit-profile/edit-phone-modal/edit-phone-modal.tsx
+++ src/app/components/messenger-page/settings/edit-profile/edit-phone-modal/edit-phone-modal.tsx
@@ -1,7 +1,11 @@
+import { country, countryList } from 'app/common/countries';
 import Modal from 'app/components/shared/modal/modal';
 import WithBackground from 'app/components/shared/with-background';
-import React from 'react';
+import { parsePhoneNumberFromString } from 'libphonenumber-js';
+import React, { useCallback, useRef, useState } from 'react';
 import './edit-phone-modal.scss';
+import ModalCountrySelect from './modal-country-select/modal-country-select';
+import ModalPhoneInput from './modal-phone-input/modal-phone-input';
 
 namespace EditPhoneModal {
 	export interface Props {
@@ -11,13 +15,54 @@ namespace EditPhoneModal {
 }
 
 const EditPhoneModal = ({ close, isDisplayed }: EditPhoneModal.Props) => {
+	const [country, setCountry] = useState<country>(countryList[230]);
+	const [phone, setPhone] = useState<string>('');
+
+	const [countrySelectRef, setCountrySelectRef] = useState<React.RefObject<HTMLInputElement> | null>(null);
+	const phoneInputRef = useRef<HTMLInputElement>(null);
+
+	const sendSms = useCallback(() => {
+		const phoneNumber = parsePhoneNumberFromString(phone);
+		//!TODO: Replace here with send sms code logic
+		console.log(phoneNumber);
+	}, [phone]);
+
+	const displayCountries = useCallback(() => {
+		countrySelectRef?.current?.focus();
+		var clickEvent = document.createEvent('MouseEvents');
+		clickEvent.initEvent('mousedown', true, true);
+		countrySelectRef?.current?.dispatchEvent(clickEvent);
+	}, [countrySelectRef]);
+
+	const focusPhoneInput = useCallback(() => {
+		phoneInputRef.current?.focus();
+	}, [phoneInputRef]);
+
 	return (
 		<WithBackground isBackgroundDisplayed={isDisplayed} onBackgroundClick={close}>
 			<Modal
 				isDisplayed={isDisplayed}
 				title={'Edit Phone Number'}
 				closeModal={close}
-				contents={<div className={'edit-phone-modal'}></div>}
+				contents={
+					<div className={'edit-phone-modal'}>
+						<ModalCountrySelect
+							setRef={setCountrySelectRef}
+							country={country}
+							setCountry={setCountry}
+							setPhone={setPhone}
+							focusPhoneInput={focusPhoneInput}
+						/>
+						<ModalPhoneInput
+							ref={phoneInputRef}
+							displayCountries={displayCountries}
+							country={country}
+							phone={phone}
+							setPhone={setPhone}
+							sendSms={sendSms}
+						/>
+					</div>
+				}
 				buttons={[
 					{
 						text: 'Save',
@@ -27,7 +72,7 @@ const EditPhoneModal = ({ close, isDisplayed }: EditPhoneModal.Props) => {
 							padding: '11px 0px',
 							border: '1px solid rgb(215, 216, 217)',
 							width: '100%',
-							marginBottom: '-6px',
+							marginBottom: '10px',
 						},
 
 						position: 'left',
---
+++ src/app/components/messenger-page/settings/edit-profile/edit-phone-modal/modal-phone-input/modal-phone-input.tsx
@@ -0,0 +1,61 @@
+import React, { useCallback } from 'react';
+import './modal-phone-input.scss';
+import { country } from 'app/common/countries';
+import { AsYouType } from 'libphonenumber-js';
+
+namespace ModalPhoneInput {
+	export interface Props {
+		country: country;
+		phone: string;
+		setPhone: Function;
+		displayCountries: () => void;
+		sendSms: () => void;
+	}
+}
+
+const ModalPhoneInput = React.forwardRef(
+	(
+		{ country, phone, setPhone, displayCountries, sendSms }: ModalPhoneInput.Props,
+		ref: React.Ref<HTMLInputElement>,
+	) => {
+		const trimCountryCode = useCallback((countryCode: string, phone: string) => {
+			let regex = '';
+			const countryCodeArr = String(countryCode).split('');
+
+			countryCodeArr.forEach((char) => {
+				regex += `[${char}]\\s?`;
+			});
+
+			const replaceRegex = new RegExp(regex);
+
+			return phone.replace(replaceRegex, '');
+		}, []);
+
+		return (
+			<div className='modal-phone-input'>
+				<div className='modal-phone-input__label'>Phone Number</div>
+				<div className='modal-phone-input__inputs-wrapper'>
+					<input
+						onClick={displayCountries}
+						type='text'
+						className='modal-phone-input__country-code'
+						readOnly
+						value={country.number}
+					/>
+					<input
+						ref={ref}
+						placeholder={'234-56-789'}
+						value={trimCountryCode(country.number, new AsYouType().input(phone))}
+						onChange={(e) => {
+							setPhone(new AsYouType().input(country.number + e.target.value));
+						}}
+						className='modal-phone-input__input'
+						onKeyDown={(event: React.KeyboardEvent<HTMLInputElement>) => event.key === 'Enter' && sendSms()}
+					/>
+				</div>
+			</div>
+		);
+	},
+);
+
+export default ModalPhoneInput;

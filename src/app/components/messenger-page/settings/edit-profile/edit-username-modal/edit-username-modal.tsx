5aff6662169cb639ebfec381c2a11cf9a624cd3c
eternalEagle <52489178+eternalEagle@users.noreply.github.com>
2020-10-21 19:03:39 +0300

    EditUserNameModal done

--- src/app/components/messenger-page/settings/edit-profile/edit-username-modal/edit-username-modal.tsx
+++ src/app/components/messenger-page/settings/edit-profile/edit-username-modal/edit-username-modal.tsx
@@ -1,6 +1,11 @@
 import Modal from 'app/components/shared/modal/modal';
 import WithBackground from 'app/components/shared/with-background';
-import React from 'react';
+import { getMyProfileSelector } from 'app/store/my-profile/selectors';
+import React, { useState } from 'react';
+import { useCallback } from 'react';
+import { useSelector } from 'react-redux';
+import ValidSvg from 'app/assets/icons/ic-check-filled.svg';
+import InValidSvg from 'app/assets/icons/ic-dismiss.svg';
 import './edit-username-modal.scss';
 
 namespace EditUserNameModal {
@@ -11,13 +16,48 @@ namespace EditUserNameModal {
 }
 
 const EditUserNameModal = ({ close, isDisplayed }: EditUserNameModal.Props) => {
+	const myProfile = useSelector(getMyProfileSelector);
+
+	const [userName, setUserName] = useState(myProfile?.nickname || '');
+
+	const changeUserName = useCallback(
+		(event: React.ChangeEvent<HTMLInputElement>) => {
+			setUserName(event.target.value);
+		},
+		[setUserName],
+	);
 	return (
 		<WithBackground isBackgroundDisplayed={isDisplayed} onBackgroundClick={close}>
 			<Modal
 				isDisplayed={isDisplayed}
 				title={'Edit Username'}
 				closeModal={close}
-				contents={<div className={'edit-username-modal'}></div>}
+				contents={
+					<div className={'edit-username-modal'}>
+						<div className='edit-username-modal__input-block'>
+							<span className='edit-username-modal__input-label'>Username</span>
+							<div className='edit-username-modal__input-wrapper'>
+								<input
+									value={userName}
+									onChange={changeUserName}
+									type='text'
+									className='edit-username-modal__input'
+								/>
+								{false && (
+									<div className='edit-username-modal__valid'>
+										<ValidSvg viewBox='0 0 25 25' />
+									</div>
+								)}
+								{true && (
+									<div className='edit-username-modal__invalid'>
+										<InValidSvg viewBox='0 0 25 25' />
+									</div>
+								)}
+							</div>
+						</div>
+						<span className='edit-username-modal__requirements'>You can use a-z, 0-9 and underscores.</span>
+					</div>
+				}
 				buttons={[
 					{
 						text: 'Save',
@@ -27,7 +67,8 @@ const EditUserNameModal = ({ close, isDisplayed }: EditUserNameModal.Props) => {
 							padding: '11px 0px',
 							border: '1px solid rgb(215, 216, 217)',
 							width: '100%',
-							marginBottom: '-6px',
+							marginBottom: '10px',
+							marginTop: '-5px',
 						},
 
 						position: 'left',

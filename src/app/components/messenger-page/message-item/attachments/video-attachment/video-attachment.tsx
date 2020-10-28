581f98de98d9372e0e1fba6d51c46a029fb4c159
eternalEagle <52489178+eternalEagle@users.noreply.github.com>
2020-10-29 00:09:06 +0200

    Big photo/big video done

--- src/app/components/messenger-page/message-item/attachments/video-attachment/video-attachment.tsx
+++ src/app/components/messenger-page/message-item/attachments/video-attachment/video-attachment.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import './video-attachment.scss';
 
 import PlaySvg from 'app/assets/icons/ic-play.svg';
 import { VideoBase } from 'app/store/messages/models';
 import moment from 'moment';
+import FadeAnimationWrapper from 'app/components/shared/fade-animation-wrapper/fade-animation-wrapper';
+import VideoPlayer from './video-player/video-player';
 
 namespace VideoAttachment {
 	export interface Props {
@@ -12,14 +14,24 @@ namespace VideoAttachment {
 }
 
 const VideoAttachment = ({ attachment }: VideoAttachment.Props) => {
+	const [videoPlayerDisplayed, setVideoPlayerDisplayed] = useState(false);
+	const changeVideoPlayerDisplayed = useCallback(() => {
+		setVideoPlayerDisplayed((oldState) => !oldState);
+		console.log('78');
+	}, [setVideoPlayerDisplayed]);
+
 	return (
-		<div className='video-attachment'>
+		<div onClick={changeVideoPlayerDisplayed} className='video-attachment'>
 			<img src={attachment.firstFrameUrl} alt='' className='video-attachment__img' />
 			<div className='video-attachment__blur'></div>
 			<PlaySvg className='video-attachment__svg' viewBox='0 0 25 25' />
 			<div className='video-attachment__duration'>
 				{moment.utc(attachment.durationInSeconds * 1000).format('mm:ss')}
 			</div>
+
+			<FadeAnimationWrapper isDisplayed={videoPlayerDisplayed}>
+				<VideoPlayer url={attachment.url} onClose={changeVideoPlayerDisplayed} />
+			</FadeAnimationWrapper>
 		</div>
 	);
 };

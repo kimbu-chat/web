f356510e09d0a66b7c5868441c3a4b35531773ca
eternalEagle <52489178+eternalEagle@users.noreply.github.com>
2020-11-08 11:28:03 +0200

    File download progress work started

--- src/app/utils/file-download.tsx
+++ src/app/utils/file-download.tsx
@@ -1,26 +1,24 @@
-const forceDownload = (blob: string, filename: string) => {
-	var a = document.createElement('a');
-	a.download = filename;
-	a.href = blob;
-	// For Firefox https://stackoverflow.com/a/32226068
-	document.body.appendChild(a);
-	a.click();
-	a.remove();
-};
+export const fileDownload = (url: string, fileName: string, onProgress?: (loaded: number, total: number) => void) => {
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', url, true);
+	xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
+	xhr.responseType = 'blob';
+	if (onProgress) {
+		xhr.onprogress = (progressEvent: ProgressEvent<EventTarget>) => {
+			onProgress(progressEvent.loaded, progressEvent.total);
+		};
+	}
+	xhr.onload = function () {
+		var urlCreator = window.URL || window.webkitURL;
+		var imageUrl = urlCreator.createObjectURL(this.response);
+		var tag = document.createElement('a');
+		tag.href = imageUrl;
+		tag.download = fileName;
+		document.body.appendChild(tag);
+		tag.click();
+		document.body.removeChild(tag);
+	};
+	xhr.send();
 
-// Current blob size limit is around 500MB for browsers
-export const fileDownload = (url: string, filename: string) => {
-	if (!filename) filename = url.split('\\').pop()!.split('/').pop()!;
-	fetch(url, {
-		headers: new Headers({
-			Origin: location.origin,
-		}),
-		mode: 'cors',
-	})
-		.then((response) => response.blob())
-		.then((blob) => {
-			let blobUrl = window.URL.createObjectURL(blob);
-			forceDownload(blobUrl, filename);
-		})
-		.catch((e) => console.error(e));
+	return xhr.abort;
 };

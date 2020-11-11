90344f4c55e47278bac2e1a78e020a1cf8904fdf
eternalEagle <52489178+eternalEagle@users.noreply.github.com>
2020-11-11 22:23:39 +0200

    #116

--- src/app/components/login-page/phone-confirmation/phone-confirmation.tsx
+++ src/app/components/login-page/phone-confirmation/phone-confirmation.tsx
@@ -107,7 +107,7 @@ const PhoneConfirmation: React.FC<PhoneConfirmation.Props> = ({ preloadNext }) =
 		<>
 			<div className='phone-confirmation'>
 				<div className='phone-confirmation__container'>
-					<img src='' alt='' className='phone-confirmation__logo' />
+					<h1 className='phone-confirmation__logo'>RAVUDI</h1>
 					<p onClick={changeNumbersDisplayedState} className='phone-confirmation__confirm-phone'>
 						{t('loginPage.confirm_phone')}
 					</p>

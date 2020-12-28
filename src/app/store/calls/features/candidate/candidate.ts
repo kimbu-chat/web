72797bab1a5fe36651ed20887c24eb0ddab80465
eternalEagle <52489178+eternalEagle@users.noreply.github.com>
2020-12-28 18:20:02 +0200

    works but iceCandidate error sometimes appears

--- src/app/store/calls/features/candidate/candidate.ts
+++ src/app/store/calls/features/candidate/candidate.ts
@@ -3,7 +3,6 @@ import { SagaIterator } from 'redux-saga';
 import { call, select } from 'redux-saga/effects';
 import { createAction } from 'typesafe-actions';
 import { getCallInterlocutorIdSelector } from '../../selectors';
-import { ignoreOffer } from '../../utils/user-media';
 import { CandidateActionPayload } from './candidate-action-payload';
 
 export class Candidate {
@@ -15,14 +14,8 @@ export class Candidate {
     return function* candidateSaga(action: ReturnType<typeof Candidate.action>): SagaIterator {
       const interlocutorId = yield select(getCallInterlocutorIdSelector);
 
-      try {
-        if (action.payload.userInterlocutorId === interlocutorId) {
-          yield call(async () => await peerConnection?.addIceCandidate(new RTCIceCandidate(action.payload.candidate)));
-        }
-      } catch (err) {
-        if (!ignoreOffer) {
-          throw err;
-        } // Suppress ignored offer's candidates
+      if (action.payload.userInterlocutorId === interlocutorId) {
+        yield call(async () => await peerConnection?.addIceCandidate(new RTCIceCandidate(action.payload.candidate)));
       }
     };
   }

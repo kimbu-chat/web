dd0c72ca19eb70ea7a6341a26bf558b336479e75
eternalEagle <52489178+eternalEagle@users.noreply.github.com>
2021-01-09 22:13:43 +0200

    feat(#299)

--- src/app/components/shared/internet-error/internet-error.tsx
+++ src/app/components/shared/internet-error/internet-error.tsx
@@ -1,20 +1,9 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
-import { getInternetStateSelector } from 'store/internet/selectors';
 
 import './internet-error.scss';
 
-export const InternetError = React.memo(() => {
-  const internetState = useSelector(getInternetStateSelector);
-  return (
-    <>
-      {internetState ? (
-        <div key={1} hidden />
-      ) : (
-        <div key={2} className='internet-state internet-state--offline'>
-          <p>Internet disconnected</p>
-        </div>
-      )}
-    </>
-  );
-});
+export const InternetError = React.memo(() => (
+  <div className='internet-error'>
+    <p>Internet disconnected</p>
+  </div>
+));

108f50558ad44db12576e95cb2a639a146db4174
Artem Avanesov <avan0619@gmail.com>
2021-02-08 00:25:18 +0300

    cleanup

--- src/app/store/auth/common/utils/get-push-notification-tokens.ts
+++ src/app/store/auth/common/utils/get-push-notification-tokens.ts
@@ -1,25 +1,25 @@
 import { messaging } from '../../../middlewares/firebase/firebase';
 
 export async function getPushNotificationToken(): Promise<string | null> {
+  if (!('serviceWorker' in navigator)) {
+    return null;
+  }
+
   if (Notification.permission === 'denied' || Notification.permission === 'default') {
     await Notification.requestPermission();
   }
 
   if (Notification.permission === 'granted') {
-    if ('serviceWorker' in navigator) {
-      try {
-        const registration = await navigator.serviceWorker.register('./firebase-messaging-sw.js');
-        const tokenId: string | undefined = await messaging?.getToken({ serviceWorkerRegistration: registration });
-        if (tokenId) {
-          return tokenId;
-        }
-        return null;
-      } catch {
-        return null;
+    try {
+      const registration = await navigator.serviceWorker.register('./firebase-messaging-sw.js');
+      const tokenId: string | undefined = await messaging?.getToken({ serviceWorkerRegistration: registration });
+      if (tokenId) {
+        return tokenId;
       }
+      return null;
+    } catch {
+      return null;
     }
-
-    return null;
   }
 
   return null;

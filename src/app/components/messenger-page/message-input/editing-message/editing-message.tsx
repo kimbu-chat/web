1fd489a9cc45affc30c1c96129e3d971672322a5
Stef <sstefstefv@gmail.com>
2021-03-16 11:33:17 +0200

    feat: Edit UI added

--- src/app/components/messenger-page/message-input/editing-message/editing-message.tsx
+++ src/app/components/messenger-page/message-input/editing-message/editing-message.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import './editing-message.scss';
 
-const EditingMessage: React.FC = () => {
-  const str = 'hi';
-  return <div className='editing-message'>{str}</div>;
-};
+import CloseSvg from 'icons/close.svg';
+import CrayonSvg from 'icons/crayon.svg';
+import { useSelector } from 'react-redux';
+import { useActionWithDispatch } from 'app/hooks/use-action-with-dispatch';
+import { getMessageToEditSelector } from 'app/store/chats/selectors';
+import { ResetEditMessage } from 'app/store/chats/features/edit-message/reset-edit-message';
 
-EditingMessage.displayName = 'EditingMessage';
+export const EditingMessage = React.memo(() => {
+  const editingMessage = useSelector(getMessageToEditSelector);
 
-export { EditingMessage };
+  const resetEditToMessage = useActionWithDispatch(ResetEditMessage.action);
+
+  return (
+    <div className='editing-message'>
+      <CrayonSvg className='editing-message__icon' viewBox='0 0 16 16' />
+      <div className='editing-message__line' />
+
+      <div className='editing-message__message-contents'>{editingMessage?.text}</div>
+      <button type='button' onClick={resetEditToMessage} className='editing-message__close'>
+        <CloseSvg viewBox='0 0 24 24' />
+      </button>
+    </div>
+  );
+});

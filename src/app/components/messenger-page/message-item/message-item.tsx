4288fc698e2cd8b0292e4cbe7a43d13cea227a56
Stef <sstefstefv@gmail.com>
2021-03-17 15:59:50 +0200

    system message restyle

--- src/app/components/messenger-page/message-item/message-item.tsx
+++ src/app/components/messenger-page/message-item/message-item.tsx
@@ -9,13 +9,23 @@ import { useActionWithDispatch } from 'app/hooks/use-action-with-dispatch';
 import { getIsSelectMessagesStateSelector } from 'store/chats/selectors';
 import { Avatar } from 'components';
 import { getUserInitials } from 'app/utils/interlocutor-name-utils';
-import { IUser } from 'app/store/common/models';
+import { CallStatus, IUser } from 'app/store/common/models';
 import moment from 'moment';
+
 import CrayonSvg from 'icons/crayon.svg';
+import LeaveSvg from 'icons/leave.svg';
+import CreateChatSvg from 'icons/create-chat.svg';
+import AddUsersSvg from 'icons/add-users.svg';
+import OutgoingCallSvg from 'icons/outgoing-call.svg';
+import IncomingCallSvg from 'icons/incoming-call.svg';
+import MissedCallSvg from 'icons/missed-call.svg';
+import DeclinedCallSvg from 'icons/declined-call.svg';
+import PictureSvg from 'icons/picture.svg';
 
 import MessageQeuedSvg from 'icons/message-queued.svg';
 import MessageSentSvg from 'icons/message-sent.svg';
 import MessageReadSvg from 'icons/message-read.svg';
+
 import SelectSvg from 'icons/select.svg';
 import {
   IRawAttachment,
@@ -112,11 +122,35 @@ const MessageItem: React.FC<IMessageItemProps> = React.memo(
     );
 
     if (message?.systemMessageType !== SystemMessageType.None) {
+      const additionalData = JSON.parse(message.text);
+      const callStatus = additionalData?.status;
+      const isOutgoing = myId === additionalData?.userCallerId;
       return (
         <>
-          <div className='message__separator'>
-            <span>{MessageUtils.constructSystemMessageText(message as IMessage, t, myId)}</span>
+          <div className='message__system-message'>
+            <div
+              className={`message__system-message__content ${callStatus === CallStatus.Ended && 'message__system-message__content--success-call'} ${
+                (callStatus === CallStatus.Declined || callStatus === CallStatus.NotAnswered || callStatus === CallStatus.Interrupted) &&
+                'message__system-message__content--failure-call'
+              }`}
+            >
+              <div className='message__system-message__icon'>
+                {message.systemMessageType === SystemMessageType.GroupChatMemberAdded && <AddUsersSvg viewBox='0 0 18 18' />}
+                {message.systemMessageType === SystemMessageType.GroupChatMemberRemoved && <LeaveSvg viewBox='0 0 18 18' />}
+                {message.systemMessageType === SystemMessageType.GroupChatCreated && <CreateChatSvg viewBox='0 0 24 24' />}
+                {message.systemMessageType === SystemMessageType.GroupChatNameChanged && <CrayonSvg viewBox='0 0 16 16' />}
+                {message.systemMessageType === SystemMessageType.GroupChatAvatarChanged && <PictureSvg viewBox='0 0 18 19' />}
+
+                {(message.systemMessageType === SystemMessageType.CallEnded &&
+                  callStatus === CallStatus.Ended &&
+                  (isOutgoing ? <OutgoingCallSvg viewBox='0 0 11 12' /> : <IncomingCallSvg viewBox='0 0 12 12' />)) ||
+                  (callStatus === CallStatus.NotAnswered && <MissedCallSvg viewBox='0 0 12 12' />) ||
+                  ((callStatus === CallStatus.Declined || callStatus === CallStatus.Interrupted) && <DeclinedCallSvg viewBox='0 0 13 14' />)}
+              </div>
+              <span>{MessageUtils.constructSystemMessageText(message as IMessage, t, myId)}</span>
+            </div>
           </div>
+
           {message.needToShowDateSeparator && (
             <div className='message__separator message__separator--date'>
               <span>{moment.utc(message.creationDateTime).local().format('dddd, MMMM D, YYYY').toString()}</span>

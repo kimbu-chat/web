b065dfeaa872a9599d23c0cfbd085d79fde50e95
eternalEagle <52489178+eternalEagle@users.noreply.github.com>
2021-04-16 18:40:40 +0300

    fix(#148): MessageRead document.title is reset on UserStatusChanged event (#185)

--- src/store/friends/sagas.ts
+++ src/store/friends/sagas.ts
@@ -4,6 +4,7 @@ import { DeleteFriend } from './features/delete-friend/delete-friend';
 import { DismissToAddContact } from './features/dismiss-to-add-contact/dismiss-to-add-contact';
 import { GetFriends } from './features/get-friends/get-friends';
 import { GetUserByPhone } from './features/get-user-by-phone/get-user-by-phone';
+import { UserStatusChangedEventHandler } from './socket-events/user-status-changed/user-status-changed-event-handler';
 
 export const FriendSagas = [
   takeLatest(GetFriends.action, GetFriends.saga),
@@ -11,4 +12,5 @@ export const FriendSagas = [
   takeEvery(AddFriend.action, AddFriend.saga),
   takeEvery(GetUserByPhone.action, GetUserByPhone.saga),
   takeEvery(DismissToAddContact.action, DismissToAddContact.saga),
+  takeEvery(UserStatusChangedEventHandler.action, UserStatusChangedEventHandler.saga),
 ];
--- src/store/friends/socket-events/user-status-changed/user-status-changed-event-handler.ts
+++ src/store/friends/socket-events/user-status-changed/user-status-changed-event-handler.ts
@@ -1,8 +1,12 @@
 import produce from 'immer';
 import { createAction } from 'typesafe-actions';
-import { getUserDraftSelector } from '../../selectors';
-import { IStatusChangedIntegrationEvent } from './status-changed-integration-event';
+import { myIdSelector } from '@store/my-profile/selectors';
+import { resetUnreadNotifications } from '@utils/set-favicon';
+import { SagaIterator } from 'redux-saga';
+import { select } from 'redux-saga/effects';
 import { IFriendsState } from '../../friends-state';
+import { IStatusChangedIntegrationEvent } from './status-changed-integration-event';
+import { getUserDraftSelector } from '../../selectors';
 
 export class UserStatusChangedEventHandler {
   static get action() {
@@ -28,5 +32,17 @@ export class UserStatusChangedEventHandler {
         return draft;
       },
     );
+  }
+
+  static get saga() {
+    return function* changeUserOnlineStatus({
+      payload,
+    }: ReturnType<typeof UserStatusChangedEventHandler.action>): SagaIterator {
+      const myId = yield select(myIdSelector);
+
+      if (payload.userId === myId) {
+        resetUnreadNotifications();
+      }
+    };
   }
 }

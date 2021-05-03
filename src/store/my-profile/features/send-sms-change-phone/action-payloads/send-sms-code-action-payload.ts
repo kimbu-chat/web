233d4e631c017914500665389b6e11b625314227
eternalEagle <sstefstefv@gmail.com>
2021-05-03 15:45:59 +0300

    feat(#217): Change phone number functionality added

---
+++ src/store/my-profile/features/send-sms-change-phone/action-payloads/send-sms-code-action-payload.ts
@@ -0,0 +1,3 @@
+export interface ISendSmsChangePhoneActionPayload {
+  phone: string;
+}
---
+++ src/store/my-profile/features/send-sms-change-phone/send-sms-change-phone.ts
@@ -0,0 +1,34 @@
+import { AxiosResponse } from 'axios';
+import { SagaIterator } from 'redux-saga';
+import { call } from 'redux-saga/effects';
+import { createAction } from 'typesafe-actions';
+import { HTTPStatusCode } from '@common/http-status-code';
+import { Meta } from '@store/common/actions';
+
+import { SendSmsCode } from '@store/auth/features/send-sms-code/send-sms-code';
+import { ISendSmsChangePhoneActionPayload } from './action-payloads/send-sms-code-action-payload';
+
+export class SendSmsChangePhone {
+  static get action() {
+    return createAction('SEND_SMS_CHANGE_PHONE')<ISendSmsChangePhoneActionPayload, Meta>();
+  }
+
+  static get saga() {
+    return function* sendSmsChangePhone(
+      action: ReturnType<typeof SendSmsChangePhone.action>,
+    ): SagaIterator {
+      const { status: codeStatus }: AxiosResponse<string> = yield call(() =>
+        SendSmsCode.httpRequest.generator({
+          phoneNumber: action.payload.phone,
+        }),
+      );
+
+      if (codeStatus !== HTTPStatusCode.OK) {
+        action?.meta.deferred.reject();
+        return;
+      }
+
+      action?.meta.deferred.resolve();
+    };
+  }
+}

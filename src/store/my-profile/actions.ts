fbc539cb96f6d6ff64db378414f905f52974db60
eternalEagle <sstefstefv@gmail.com>
2021-05-06 14:32:22 +0300

    feat(#259):current user normalized

--- src/store/my-profile/actions.ts
+++ src/store/my-profile/actions.ts
@@ -2,18 +2,15 @@ import { CancelAvatarUploading } from './features/cancel-avatar-uploading/cancel
 import { ChangeUserOnlineStatus } from './features/change-user-online-status/change-user-online-status';
 import { CheckNicknameAvailability } from './features/check-nickname-availability/check-nickname-availability';
 import { ConfirmChangePhone } from './features/confirm-change-phone/confirm-change-phone';
-import { ConfirmChangePhoneSuccess } from './features/confirm-change-phone/confirm-change-phone-success';
 import { DeactivateAccount } from './features/deactivate-account/deactivate-acccount';
 import { DeleteAccount } from './features/delete-account/delete-account';
 import { GetMyProfile } from './features/get-my-profile/get-my-profile';
 import { GetMyProfileSuccess } from './features/get-my-profile/get-my-profile-success';
 import { SendSmsChangePhone } from './features/send-sms-change-phone/send-sms-change-phone';
 import { UpdateMyProfile } from './features/update-my-profile/update-my-profile';
-import { UpdateMyProfileSuccess } from './features/update-my-profile/update-my-profile-success';
 import { UploadAvatar } from './features/upload-avatar/upload-avatar';
 
 export const updateMyProfileAction = UpdateMyProfile.action;
-export const updateMyProfileSuccessAction = UpdateMyProfileSuccess.action;
 export const checkNicknameAvailabilityAction = CheckNicknameAvailability.action;
 export const getMyProfileAction = GetMyProfile.action;
 export const getMyProfileSuccessAction = GetMyProfileSuccess.action;
@@ -24,11 +21,9 @@ export const deactivateAccountAction = DeactivateAccount.action;
 export const deleteAccountAction = DeleteAccount.action;
 export const sendSmsChangePhone = SendSmsChangePhone.action;
 export const confirmChangePhone = ConfirmChangePhone.action;
-export const confirmChangePhoneSuccess = ConfirmChangePhoneSuccess.action;
 
 export const MyProfileActions = {
   updateMyProfileAction,
-  updateMyProfileSuccessAction,
   checkNicknameAvailabilityAction,
   getMyProfileAction,
   getMyProfileSuccessAction,
@@ -39,5 +34,4 @@ export const MyProfileActions = {
   deactivateAccountAction,
   sendSmsChangePhone,
   confirmChangePhone,
-  confirmChangePhoneSuccess,
 };
--- src/store/users/reducer.ts
+++ src/store/users/reducer.ts
@@ -1,7 +1,6 @@
 import { UserDeactivatedEventHandler } from '@store/users/socket-events/user-deactivated/user-deactivated-event-handler';
 import { UserDeletedEventHandler } from '@store/users/socket-events/user-deleted/user-deleted';
 import { UserPhoneNumberChangedEventHandler } from '@store/users/socket-events/user-phone-number-changed/user-phone-number-changed';
-import produce from 'immer';
 import { createReducer } from 'typesafe-actions';
 import { UserEditedEventHandler } from './socket-events/user-edited/user-edited-event-handler';
 import { UpdateUsersList } from './features/update-users-list/update-users-list';
@@ -20,20 +19,6 @@ const reducer = createReducer<IUsersState>(initialState)
   .handleAction(UserDeletedEventHandler.action, UserDeletedEventHandler.reducer)
   .handleAction(
     UserPhoneNumberChangedEventHandler.action,
-    produce(
-      (
-        draft: IUsersState,
-        { payload }: ReturnType<typeof UserPhoneNumberChangedEventHandler.action>,
-      ) => {
-        const { userId, phoneNumber } = payload;
-        const user = draft.users[userId];
-
-        if (user) {
-          user.phoneNumber = phoneNumber;
-        }
-
-        return draft;
-      },
-    ),
+    UserPhoneNumberChangedEventHandler.reducer,
   );
 export default reducer;

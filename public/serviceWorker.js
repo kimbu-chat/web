c568c810905437f2e31e3e18e7a327dff0144358
skstef <52489178+eternalEagle@users.noreply.github.com>
2021-06-23 13:32:07 +0300

    feat(#398): PWA added

---
+++ public/serviceWorker.js
@@ -0,0 +1,24 @@
+let CACHE_NAME = 'my-site-cache-v1';
+
+const urlsToCache = ['/', '/index.html'];
+
+self.addEventListener('install', function (event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(urlsToCache);
+    }),
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    }),
+  );
+});

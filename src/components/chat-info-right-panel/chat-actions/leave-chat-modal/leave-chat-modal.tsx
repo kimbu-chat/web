80d3127967f987bc228ab9e44909788376bd6934
skstef <52489178+eternalEagle@users.noreply.github.com>
2021-07-10 22:08:01 +0300

    fix(#537): ui fixed

--- src/components/chat-info-right-panel/chat-actions/leave-chat-modal/leave-chat-modal.tsx
+++ src/components/chat-info-right-panel/chat-actions/leave-chat-modal/leave-chat-modal.tsx
@@ -1,8 +1,6 @@
 import React, { useCallback, useState } from 'react';
 
-import classNames from 'classnames';
 import { useTranslation } from 'react-i18next';
-import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 
 import { Button } from '@components/button';
@@ -10,7 +8,6 @@ import { Modal } from '@components/modal';
 import { useEmptyActionWithDeferred } from '@hooks/use-action-with-deferred';
 import { INSTANT_MESSAGING_PATH } from '@routing/routing.constants';
 import { leaveGroupChatAction } from '@store/chats/actions';
-import { getSelectedGroupChatNameSelector } from '@store/chats/selectors';
 
 import './leave-chat-modal.scss';
 
@@ -25,8 +22,6 @@ export const LeaveChatModal: React.FC<ILeaveChatModalProps> = ({ hide }) => {
   const history = useHistory();
 
   const [loading, setLoading] = useState(false);
-
-  const selectedGroupChatName = useSelector(getSelectedGroupChatNameSelector);
 
   const leaveGroupChat = useEmptyActionWithDeferred(leaveGroupChatAction);
 
@@ -43,25 +38,7 @@ export const LeaveChatModal: React.FC<ILeaveChatModalProps> = ({ hide }) => {
       <>
         <Modal.Header>{t('chatActions.leave-chat')}</Modal.Header>
         <div className={BLOCK_NAME}>
-          <div className={`${BLOCK_NAME}__сontent`}>
-            {t('chatInfo.leave-confirmation', { groupChatName: selectedGroupChatName })
-              .split(selectedGroupChatName || '')
-              .map((text, index, arr) => (
-                // eslint-disable-next-line react/no-array-index-key
-                <React.Fragment key={index}>
-                  <span className={`${BLOCK_NAME}__сontent__text`}>{text}</span>
-                  {index < arr.length - 1 && (
-                    <span
-                      className={classNames(
-                        `${BLOCK_NAME}__сontent__text`,
-                        `${BLOCK_NAME}__сontent__text--highlighted`,
-                      )}>
-                      {selectedGroupChatName}
-                    </span>
-                  )}
-                </React.Fragment>
-              ))}
-          </div>
+          <div className={`${BLOCK_NAME}__сontent`}>{t('chatInfo.leave-confirmation')}</div>
           <div className={`${BLOCK_NAME}__btn-block`}>
             <button type="button" className={`${BLOCK_NAME}__cancel-btn`} onClick={hide}>
               {t('chatInfo.cancel')}

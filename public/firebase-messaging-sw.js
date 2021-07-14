a20c28f6f82cca10d3782732510a1f844566eefd
skstef <52489178+eternalEagle@users.noreply.github.com>
2021-07-14 19:17:53 +0300

    fix: interim

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -9,6 +9,31 @@ const config = {
   appId: '1:104186841570:web:d96d94528e19aa12d5d1bb',
 };
 
+self.addEventListener('notificationclick', function (event) {
+  console.log('On notification click: ', event.notification.tag);
+  event.notification.close();
+
+  // This looks to see if the current is already open and
+  // focuses if it is
+  event.waitUntil(
+    clients
+      .matchAll({
+        type: 'window',
+      })
+      .then(function (clientList) {
+        for (var i = 0; i < clientList.length; i++) {
+          var client = clientList[i];
+          if (client.url == '/' && 'focus' in client) return client.focus();
+        }
+        if (clients.openWindow) return clients.openWindow('/');
+      }),
+  );
+});
+
 firebase.initializeApp(config);
 
-firebase.messaging();
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  console.log('Message received. ', payload.data);
+});
--- src/service-worker.ts
+++ src/service-worker.ts
@@ -81,6 +81,7 @@ registerRoute(
 // This allows the web app to trigger skipWaiting via
 // registration.waiting.postMessage({type: 'SKIP_WAITING'})
 self.addEventListener('message', (event) => {
+  console.log(event);
   if (event.data && event.data.type === 'SKIP_WAITING') {
     self.skipWaiting();
   }

499be4bdf3c3f345d024417c84a5efc791ba81ba
skstef <sstefstefv@gmail.com>
2021-07-21 15:23:14 +0300

    refactor(#615): slider in settings refactored

--- src/pages/settings/appearance/appearance.tsx
+++ src/pages/settings/appearance/appearance.tsx
@@ -1,5 +1,6 @@
 import React, { useCallback } from 'react';
 
+import Slider from 'rc-slider/lib/Slider';
 import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
 
@@ -29,6 +30,37 @@ const Appearance: React.FC = () => {
   const goToDarkTheme = useCallback(() => {
     changeTheme(Theme.DARK);
   }, [changeTheme]);
+
+  const getFontLabel = (size: number) => (
+    <span
+      className={`appearance__font-size appearance__font-size--${size} ${
+        size === fontSize ? 'appearance__font-size--active' : ''
+      }`}>{`${size} pt`}</span>
+  );
+
+  const handleStyle: React.CSSProperties = {
+    background: '#3f8ae0',
+    border: '4px solid #fff',
+    borderRadius: '50%',
+    bottom: '-4px',
+    boxSizing: 'border-box',
+    height: '28px',
+    left: '50%',
+    position: 'absolute',
+    transform: 'translateX(-50%)',
+    width: '28px',
+    cursor: 'pointer',
+  };
+
+  const railStyle: React.CSSProperties = {
+    background: '#3f8ae0',
+    borderRadius: '6px',
+    boxShadow:
+      'inset -2px 2px 4px rgba(46, 101, 164, 0.2), inset 2px -2px 4px rgba(46, 101, 164, 0.2), inset -2px -2px 4px rgba(80, 175, 255, 0.9), inset 2px 2px 5px rgba(46, 101, 164, 0.9)',
+    height: '20px',
+    marginTop: '14px',
+    cursor: 'pointer',
+  };
 
   return (
     <div className="appearance">
@@ -65,75 +97,26 @@ const Appearance: React.FC = () => {
       </div>
       <HorizontalSeparator />
       <h3 className="appearance__font-size-title">{t('appearance.font-size')}</h3>
-      <div className="appearance__font-size-box">
-        <div className="appearance__font-sizes">
-          <button
-            onClick={() => changeFontSize(12)}
-            type="button"
-            className={`appearance__font-size appearance__font-size--12 ${
-              fontSize === 12 ? 'appearance__font-size--active' : ''
-            }`}>
-            <span>12 pt</span>
-            <div
-              className={`appearance__font-progress-circle  ${
-                fontSize === 12 ? '' : 'appearance__font-progress-circle--transparent'
-              }`}
-            />
-          </button>
-          <button
-            onClick={() => changeFontSize(14)}
-            type="button"
-            className={`appearance__font-size appearance__font-size--14 ${
-              fontSize === 14 ? 'appearance__font-size--active' : ''
-            }`}>
-            <span>14 pt</span>
-            <div
-              className={`appearance__font-progress-circle  ${
-                fontSize === 14 ? '' : 'appearance__font-progress-circle--transparent'
-              }`}
-            />
-          </button>
-          <button
-            onClick={() => changeFontSize(16)}
-            type="button"
-            className={`appearance__font-size appearance__font-size--16 ${
-              fontSize === 16 ? 'appearance__font-size--active' : ''
-            }`}>
-            <span>16 pt</span>
-            <div
-              className={`appearance__font-progress-circle  ${
-                fontSize === 16 ? '' : 'appearance__font-progress-circle--transparent'
-              }`}
-            />
-          </button>
-          <button
-            onClick={() => changeFontSize(18)}
-            type="button"
-            className={`appearance__font-size appearance__font-size--18 ${
-              fontSize === 18 ? 'appearance__font-size--active' : ''
-            }`}>
-            <span>18 pt</span>
-            <div
-              className={`appearance__font-progress-circle  ${
-                fontSize === 18 ? '' : 'appearance__font-progress-circle--transparent'
-              }`}
-            />
-          </button>
-          <button
-            onClick={() => changeFontSize(24)}
-            type="button"
-            className={`appearance__font-size appearance__font-size--24 ${
-              fontSize === 24 ? 'appearance__font-size--active' : ''
-            }`}>
-            <span>24 pt</span>
-            <div
-              className={`appearance__font-progress-circle  ${
-                fontSize === 24 ? '' : 'appearance__font-progress-circle--transparent'
-              }`}
-            />
-          </button>
-        </div>
-        <div className="appearance__font-progress" />
+
+      <div className="appearance__font-sizes">
+        <Slider
+          handleStyle={handleStyle}
+          railStyle={railStyle}
+          value={fontSize}
+          min={11.25}
+          max={25.1}
+          step={2}
+          marks={{
+            12: getFontLabel(12),
+            14: getFontLabel(14),
+            16: getFontLabel(16),
+            18: getFontLabel(18),
+            24: getFontLabel(24),
+          }}
+          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+          // @ts-ignore
+          onChange={changeFontSize}
+        />
       </div>
     </div>
   );

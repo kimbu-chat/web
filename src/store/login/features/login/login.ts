ab8c5e6d97a0a2d87863b540796b0f0045eb3028
skstef <sstefstefv@gmail.com>
2021-07-27 15:23:35 +0300

    feat(#640): firebase is lazy loaded on login success

--- src/store/login/features/login/login.ts
+++ src/store/login/features/login/login.ts
@@ -1,15 +1,13 @@
 import { AxiosResponse } from 'axios';
 import jwtDecode from 'jwt-decode';
 import { SagaIterator } from 'redux-saga';
-import { call, apply, put, spawn } from 'redux-saga/effects';
+import { call, apply, put } from 'redux-saga/effects';
 import { createAction } from 'typesafe-actions';
 
 import { HTTPStatusCode } from '@common/http-status-code';
 import { MAIN_API } from '@common/paths';
 import { AuthService } from '@services/auth-service';
 import { authRequestFactory, HttpRequestMethod } from '@store/common/http';
-import { messaging } from '@store/middlewares/firebase/firebase';
-import { SubscribeToPushNotifications } from '@store/notifications/features/subscribe-to-push-notifications/subscribe-to-push-notifications';
 
 import { ILoginActionPayload } from './action-payloads/login-action-payload';
 import { ILoginApiRequest } from './api-requests/login-api-request';
@@ -31,8 +29,6 @@ export class Login {
       );
 
       if (status === HTTPStatusCode.OK) {
-        yield call(async () => messaging?.deleteToken());
-
         const decodedJwt = jwtDecode<ICustomJwtPayload>(data.accessToken);
 
         const deviceId = decodedJwt.refreshTokenId;
@@ -40,8 +36,6 @@ export class Login {
         const authService = new AuthService();
 
         yield apply(authService, authService.initialize, [data, deviceId]);
-
-        yield spawn(SubscribeToPushNotifications.saga);
 
         yield put(LoginSuccess.action());
       }

94118f4ee564f80b6a825903e53300dee47305c9
skstef <52489178+skstef@users.noreply.github.com>
2021-07-27 16:24:35 +0300

    feat(#649): user is logged out when session is revoked (#656)

--- src/store/auth/sagas.ts
+++ src/store/auth/sagas.ts
@@ -1,13 +1,15 @@
-import { all, takeLatest, takeLeading } from 'redux-saga/effects';
+import { all, takeLeading, takeEvery } from 'redux-saga/effects';
 
 import { Logout } from './features/logout/logout';
 import { RefreshTokenSuccess } from './features/refresh-token/refresh-token-success';
 import { RefreshToken } from './features/refresh-token/refresh-token';
+import { SessionTerminatedEventHandler } from './socket-events/session-terminated/session-terminated-event-handler';
 
 export function* authSagas() {
   yield all([
-    takeLatest(Logout.action, Logout.saga),
+    takeLeading(Logout.action, Logout.saga),
     takeLeading(RefreshToken.action, RefreshToken.saga),
     takeLeading(RefreshTokenSuccess.action, RefreshTokenSuccess.saga),
+    takeEvery(SessionTerminatedEventHandler.action, SessionTerminatedEventHandler.saga),
   ]);
 }
--- src/store/auth/socket-events/session-terminated/session-terminated-event-handler.ts
+++ src/store/auth/socket-events/session-terminated/session-terminated-event-handler.ts
@@ -1,3 +1,27 @@
-export interface ISessionTerminatedIntegrationEvent {
-  deviceId: string;
+import { SagaIterator } from 'redux-saga';
+import { select, call } from 'redux-saga/effects';
+import { createAction } from 'typesafe-actions';
+
+import { deviceIdSelector } from '@store/auth/selectors';
+
+import { Logout } from '../../features/logout/logout';
+
+import { ISessionTerminatedIntegrationEvent } from './session-terminated-event';
+
+export class SessionTerminatedEventHandler {
+  static get action() {
+    return createAction('SessionTerminated')<ISessionTerminatedIntegrationEvent>();
+  }
+
+  static get saga() {
+    return function* incomingCallSaga(
+      action: ReturnType<typeof SessionTerminatedEventHandler.action>,
+    ): SagaIterator {
+      const currentDeviceId = yield select(deviceIdSelector);
+
+      if (action.payload.refreshTokenId === Number(currentDeviceId)) {
+        yield call(Logout.saga);
+      }
+    };
+  }
 }

b9bf3185c95aa531d656837e0882f45128357fa8
skstef <52489178+skstef@users.noreply.github.com>
2021-08-13 23:52:49 +0300

    fix(#757): when clicking on corner of emoji all works well (#761)

--- src/components/message-input/message-smiles/emoji/emoji.tsx
+++ src/components/message-input/message-smiles/emoji/emoji.tsx
@@ -1,30 +1,22 @@
-import React, { useCallback } from 'react';
+import React from 'react';
 
 import { IEmoji } from '@utils/smiles';
 import './emoji.scss';
 
 interface IEmojiProps {
   emoji: IEmoji;
-  onClick?: (emoji: IEmoji) => void;
 }
 
 const BLOCK_NAME = 'emoji';
 
-export const Emoji: React.FC<IEmojiProps> = ({ emoji, onClick }) => {
-  const onClickOnEmoji = useCallback(() => {
-    if (onClick) {
-      onClick(emoji);
-    }
-  }, [emoji, onClick]);
-
-  return (
-    <div onClick={onClickOnEmoji} className={BLOCK_NAME}>
-      <img
-        className={`${BLOCK_NAME}__icon`}
-        src={`https://raw.githubusercontent.com/korenskoy/emoji-data-ios/master/img-apple-64/${emoji.image}.png`}
-        alt={emoji.native}
-        loading="lazy"
-      />
-    </div>
-  );
-};
+export const Emoji: React.FC<IEmojiProps> = ({ emoji }) => (
+  <div className={BLOCK_NAME}>
+    <img
+      className={`${BLOCK_NAME}__icon`}
+      data-emoji={emoji.id}
+      src={`https://raw.githubusercontent.com/korenskoy/emoji-data-ios/master/img-apple-64/${emoji.image}.png`}
+      alt={emoji.native}
+      loading="lazy"
+    />
+  </div>
+);

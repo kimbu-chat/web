8bdf3f8fa0535f234497c28d90a6e914a9495856
Artem Avanesov <artsyom.avanesov@gmail.com>
2021-08-31 11:25:20 +0300

    fix: nickname validation the same as on server side

--- src/utils/validate-nick-name.ts
+++ src/utils/validate-nick-name.ts
@@ -1,4 +1,4 @@
 export const validateNickname = (value = '') => {
   const pattern = /^[a-z0-9_]*$/;
-  return !(!value || value.length < 5 || !value.match(pattern));
+  return !(!value || value.length < 5 || value.length > 25 || !value.match(pattern));
 };

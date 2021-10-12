a13abffa826fc3fcc23f6619c22197156cfcd5d0
Maksim Varvashevich <max270071@gmail.com>
2021-10-12 22:30:51 +0300

    feat: versioning (local - package.json, publish: github release tags) (#922)

---
+++ src/components/app-version/index.ts
@@ -0,0 +1,1 @@
+export { AppVersion } from './AppVersion';
--- src/routing/settings-router/settings-router.tsx
+++ src/routing/settings-router/settings-router.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { Switch, Redirect } from 'react-router-dom';
 
+import { AppVersion } from '@components/app-version';
 import { SettingsNavigation } from '@pages/settings/settings-navigation';
 import { mapRoutes } from '@routing/map-routing';
 import { routes } from '@routing/routes/settings-routes';
@@ -15,6 +16,7 @@ const SettingsRouter: React.FC = () => (
   <>
     <div className={`${BLOCK_NAME}__navigation`}>
       <SettingsNavigation />
+      <AppVersion className={`${BLOCK_NAME}__app-version`} />
     </div>
     <div className={`${BLOCK_NAME}__data`}>
       <Switch>

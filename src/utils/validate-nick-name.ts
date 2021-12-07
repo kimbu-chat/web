7880188ce32a4bfefaa246e0d358519434434e45
Maksim Varvashevich <max270071@gmail.com>
2021-12-07 18:57:23 +0300

    fix(#999): fix nickname validation (#1006)

--- src/utils/validate-nick-name.ts
+++ src/utils/validate-nick-name.ts
@@ -1,4 +1,4 @@
 export const validateNickname = (value = '') => {
-  const pattern = /^[a-z0-9_]*$/;
+  const pattern = /^(?!_)(?!.*_$)[a-zA-Z0-9_]+$/;
   return !(!value || value.length < 5 || value.length > 25 || !value.match(pattern));
 };

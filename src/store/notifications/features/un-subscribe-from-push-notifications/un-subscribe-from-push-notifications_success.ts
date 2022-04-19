5a50189e12378a73b9acf5ec8cfec146a6606ed2
Artem Avanesov <artsyom.avanesov@gmail.com>
2022-04-19 17:45:08 +0400

    refactor: remove unused action

--- src/store/notifications/features/un-subscribe-from-push-notifications/un-subscribe-from-push-notifications_success.ts
+++
@@ -1,7 +0,0 @@
-import { createEmptyAction } from '@store/common/actions';
-
-export class UnSubscribeToPushNotificationsSuccess {
-  static get action() {
-    return createEmptyAction('UN_SUBSCRIBE_TO_PUSH_NOTIFICATIONS_SUCCESS');
-  }
-}

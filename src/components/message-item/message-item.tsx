cdff87dde7b9c4c0ec65fea0ba1d68297a398a4f
Yan <yasinskijan161@gmail.com>
2022-08-03 23:42:59 +0300

    last

--- src/components/message-item/message-item.tsx
+++ src/components/message-item/message-item.tsx
@@ -91,13 +91,21 @@ const MessageItem: React.FC<IMessageItemProps> = React.memo(
 
     const selectThisMessage = useCallback(
       (event?: React.MouseEvent<HTMLButtonElement | HTMLDivElement, MouseEvent>) => {
+<<<<<<< HEAD
         if(isMessageQueued) {
+=======
+        if(isMessageStateInQueued) {
+>>>>>>> dca65841... was a problem, when select unsent message after any other
           return
         }
         event?.stopPropagation();
         selectMessage({ messageId });
       },
+<<<<<<< HEAD
       [messageId, selectMessage, isMessageQueued],
+=======
+      [messageId, selectMessage, isMessageStateInQueued],
+>>>>>>> dca65841... was a problem, when select unsent message after any other
     );
 
     const getMessageIcon = (): ReactElement => {

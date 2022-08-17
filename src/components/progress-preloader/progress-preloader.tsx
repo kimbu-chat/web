f72abe1d1234c22c5976a6e38b681460ee3775ed
Artsyom Ostrovskiy <ostrovskiy.a.97@gmail.com>
2022-08-17 15:40:30 +0200

    feat(#1130): add the opportunity to choose the size of the ProgressPreloader between large and small

--- src/components/progress-preloader/progress-preloader.tsx
+++ src/components/progress-preloader/progress-preloader.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { AttachmentType } from 'kimbu-models';
 
+import { LoaderSize } from '@components/loader';
 import { getRawAttachmentSizeUnit } from '@utils/get-file-size-unit';
 
 import './progress-preloader.scss';
@@ -12,23 +13,29 @@ interface IProgressPreloader {
   fileName: string;
   uploadedBytes?: number;
   byteSize: number;
+  size?: LoaderSize.SMALL | LoaderSize.LARGE;
 }
 
 const BLOCK_NAME = 'progress-preloader';
 
-const ProgressPreloader: React.FC<IProgressPreloader> = ({ progress, type, fileName, uploadedBytes, byteSize }) => (
-  <>
-    <div
-      style={{ width: `${progress}%` }}
-      className={`${BLOCK_NAME}__progress ${
-        type === AttachmentType.Picture || type === AttachmentType.Video ? `${BLOCK_NAME}__progress--photo` : ''
-      }`}
-    />
-    <div className={`${BLOCK_NAME}__data`}>
-      <div className={`${BLOCK_NAME}__title`}>{fileName}</div>
-      <div className={`${BLOCK_NAME}__size`}>{`${getRawAttachmentSizeUnit(uploadedBytes || byteSize)}/${getRawAttachmentSizeUnit(byteSize)}}`}</div>
-    </div>
-  </>
-);
+const ProgressPreloader: React.FC<IProgressPreloader> = ({ progress, type, fileName, uploadedBytes, byteSize, size = LoaderSize.SMALL }) => {
+  const byteSizeClassName = `${BLOCK_NAME}__size ${BLOCK_NAME}__size--${size}`;
+  const fileNameClassName = `${BLOCK_NAME}__title ${BLOCK_NAME}__title--${size}`;
+
+  return (
+    <>
+      <div
+        style={{ width: `${progress}%` }}
+        className={`${BLOCK_NAME}__progress ${
+          type === AttachmentType.Picture || type === AttachmentType.Video ? `${BLOCK_NAME}__progress--photo` : ''
+        }`}
+      />
+      <div className={`${BLOCK_NAME}__data`}>
+        <div className={fileNameClassName}>{fileName}</div>
+        <div className={byteSizeClassName}>{`${getRawAttachmentSizeUnit(uploadedBytes || byteSize)}/${getRawAttachmentSizeUnit(byteSize)}`}</div>
+      </div>
+    </>
+  );
+};
 
 export default ProgressPreloader;

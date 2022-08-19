3f9e7f347a6e3e95299090eb782b5d1c9c37f977
Artsyom Ostrovskiy <ostrovskiy.a.97@gmail.com>
2022-08-19 14:20:18 +0200

    feat(#1130): implement the appearance of the cross on hover in the CircleProgressPreloader

--- src/components/circle-progress-preloader/circle-progress-preloader.tsx
+++ src/components/circle-progress-preloader/circle-progress-preloader.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useRef } from 'react';
 
-import { ReactComponent as ProgressSVG } from '@icons/ic-progress-circle.svg';
-import { ReactComponent as DownloadProgressSVG } from '@icons/ic-progress.svg';
+import { ReactComponent as ProgressSVG } from '@icons/ic-progress.svg';
 
 import './circle-progress-preloader.scss';
 
@@ -17,14 +16,40 @@ export const CircleProgressPreloader: React.FC<ICircleProgressPreloader> = ({ by
   const progressSvgRef = useRef<SVGSVGElement | null>(null);
 
   useEffect(() => {
+    const element = progressSvgRef.current;
+    let handleMouseEnter: () => void;
+    let handleMouseLeave: () => void;
+
+    if (element && !withCross) {
+      const cross = element.querySelector<HTMLElement>('.cross');
+
+      if (cross) {
+        cross.style.opacity = '0';
+        cross.style.transition = 'opacity 0.1s';
+
+        handleMouseEnter = () => {
+          cross.style.opacity = '1';
+        };
+        handleMouseLeave = () => {
+          cross.style.opacity = '0';
+        };
+
+        element.addEventListener('mouseenter', handleMouseEnter);
+        element.addEventListener('mouseleave', handleMouseLeave);
+      }
+    }
+
+    return () => {
+      element?.removeEventListener('mouseenter', handleMouseEnter);
+      element?.removeEventListener('mouseleave', handleMouseLeave);
+    };
+  }, [withCross]);
+
+  useEffect(() => {
     if (progressSvgRef.current) {
       progressSvgRef.current.querySelectorAll('circle')[1].style.strokeDashoffset = String(76 - (uploadedBytes / byteSize) * 63);
     }
   }, [byteSize, progressSvgRef, uploadedBytes]);
 
-  return withCross ? (
-    <DownloadProgressSVG ref={progressSvgRef} className={`${BLOCK_NAME}__progress-svg`} />
-  ) : (
-    <ProgressSVG ref={progressSvgRef} className={`${BLOCK_NAME}__progress-svg`} />
-  );
+  return <ProgressSVG ref={progressSvgRef} className={`${BLOCK_NAME}__progress-svg`} />;
 };
